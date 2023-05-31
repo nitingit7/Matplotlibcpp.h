d439dfc9bd31947d47b1f344f444ba2af970eb81
NitinHai <119484935+NitinHai@users.noreply.github.com>
2023-05-31 09:12:19 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
+#include "matplotlibcpp.h"
 #include <vector>
-#include <algorithm>
 
-using namespace std;
+namespace plt = matplotlibcpp;
 
 int main() {
-  // Create a vector of x-coordinates.
-  vector<int> x = {1, 2, 3, 4, 5};
-
-  // Create a vector of y-coordinates.
-  vector<int> y = {2, 4, 6, 8, 10};
-
-  // Create a line object.
-  line line(x, y);
-
-  // Plot the line.
-  line.plot();
-
-  // Wait for the user to press a key.
-  cin.get();
-
-  return 0;
+  std::vector<double> y = {1, 3, 2, 4};
+  plt::plot(y);
+  plt::savefig("minimal.pdf");
 }
